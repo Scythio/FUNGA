c1e9b68425ebda7ae955dbb9b888aa5fb817852d
@rafalpalmowski <rafal.palmowski@skysnap.pl>
2022-11-14 20:17:43 +0100

    add drawer

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,24 +1,25 @@
 import React, {type PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-} from 'react-native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import {SafeAreaView, StyleSheet} from 'react-native';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 import MainScreen from './src/features/main';
 
+const Drawer = createDrawerNavigator();
+
 const App = () => {
   const backgroundStyle = {
-    backgroundColor: Colors.darker
+    backgroundColor: Colors.darker,
   };
 
   return (
-<SafeAreaView style={styles.container}>
-    <MainScreen/>
-</SafeAreaView>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Main">
+        <Drawer.Screen name="Main" component={MainScreen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 };
 
@@ -33,6 +34,6 @@ const styles = StyleSheet.create({
   },
   button: {
     marginBottom: 50,
-  }
+  },
 });
 export default App;
